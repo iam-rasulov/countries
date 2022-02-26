c156629c3c6e0f1934cf13f97381b5f990202d61
iam-rasulov <mirjalolrasulov002@gmail.com>
2022-02-27 01:30:26 +0500

    Add about page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let elbtn = document.querySelector (".btn-changer")
-let elBody = document.querySelector ("body")
+var elbtn = document.querySelector (".btn-changer")
+var elBody = document.querySelector ("body")
 
 elbtn.addEventListener("click" , function (){
     elBody.classList.toggle("bg-dark")
